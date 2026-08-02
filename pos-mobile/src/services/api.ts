71a964518e79a148cad6ba5be0d11cde6d97c3ce
kirmitatom <125992097+kirmitatom@users.noreply.github.com>
2026-08-02 15:28:01 +0300

    fixes

--- pos-mobile/src/services/api.ts
+++ pos-mobile/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Constants from "expo-constants";
 
-const API_BASE_URL = Constants.expoConfig?.extra?.API_URL || "http://192.168.1.8:4000";
+const API_BASE_URL = "http://192.168.1.12:4000";
 
 let authToken: string | null = null;
 
