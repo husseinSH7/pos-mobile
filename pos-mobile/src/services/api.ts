65be41af25cd649e5ee591f93fba4e7ac4e3e86f
husseinSH7 <husseinalshoabipr@gmail.com>
2026-08-26 14:24:38 +0300

    API was hardcoded for local testing now its in env

--- pos-mobile/src/services/api.ts
+++ pos-mobile/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Constants from "expo-constants";
 
-const API_BASE_URL = "http://192.168.1.15:4000";
+const API_BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL
 
 const API_ROOT = API_BASE_URL.endsWith("/api/v1")
   ? API_BASE_URL
